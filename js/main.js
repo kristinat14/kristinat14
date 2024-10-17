f3028a44152d5d885dc504b9dfd1c7865f9d91c7
kristinat14 <kris21082010@gmail.com>
2024-10-17 17:08:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let score = 0; 
+let many=0;
+
+const button = document.getElementById('button');
+const hamster = document.getElementById('hamster');
+
+const scoreDisplay = document.getElementById('score');
+const manyDisplay = document.getElementById('manu');
+
+hamster.addEventListener('click',() => { score++;
+
+
+
+});
+
+
+
+button.addEventListener('click',() => { 
+
+    many++;
+    manyDisplay.textContent = `Счет: ${many}`;
+
+    
+    });
+    
